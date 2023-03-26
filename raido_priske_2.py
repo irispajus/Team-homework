64adb1ff98cb189bc5954018944a8dcb6a18a41d
irispajus <124361387+irispajus@users.noreply.github.com>
2023-03-26 16:04:33 +0300

    Update raido_priske_2.py
    Tegin koodi lühemaks ja äkki ka paremini jälgitavaks.

--- raido_priske_2.py
+++ raido_priske_2.py
@@ -1,65 +1,35 @@
 #Programm, et lisada ja eemaldada automarke loendist
-#Defineerin clear funktsiooni, et puhastada terminali
-from os import system, name 
-def clear(): 
-  if name == 'nt': 
-      x = system('cls') 
-  else: 
-      x = system('clear') 
+#Defineerin clear funktsiooni, et puhastada terminali, time funktsioon on selleks, et anda kasutajale aega nimekirja ülevaatamiseks
+from os import system, name
+import time
+
+def clear():
+    if name == 'nt':
+        x = system('cls')
+    else:
+        x = system('clear')
 
 def run():
-    import time
-    
-    #Sätistan algsed andmed
-    state = 0
     array_cars = ['audi', 'bmw', 'volkswagen', 'toyota', 'kia', 'lamborghini', 'porsche', 'lexus', 'ford', 'mazda']
-    userinput = input("Sisesta automark\n")
-    
-    #Eemaldan sisestatud margi loendist
-    while state == 0:
-      if userinput in array_cars:
-          clear()
-          try:
+
+    while True:
+        userinput = input("Sisesta automark (sisesta 'exit' lõpetamiseks):\n")
+        clear()
+
+        if userinput == 'exit':
+            break
+
+        if userinput in array_cars:
             array_cars.remove(userinput)
-            pass
-          except:
-            print('Error')
-            pass
+            print("Eemaldatud automark: " + userinput + "\n")
 
-          #Prindin välja alles olevad autod
-          print ('\nAlles on jaanud:\n')
-          array_i = 0
-          
-          while array_i < len(array_cars):
-            print(array_cars[array_i])
-            array_i = array_i + 1
-            
-          state = 1
-          
-          time.sleep(2)
-          clear()
+        print("Alles olevad automargid:")
+        for car in array_cars:
+            print(car)
 
-          #Uus funktsioon, et lisada automark loendisse
-          def addBrand():
-            clear()
-            brand = str(input('\nSisesta automark, kui ei taha rohkem sisestada, kirjuta "exit"\n'))
-            if (brand == 'exit'):
-              length = len(array_cars)
-              i = 0
-              while i < length:
-                print (array_cars[i])
-                i = i + 1
-            else:
-              array_cars.append(brand)
-              print ('\nSisestasid: ' + str(brand))
-              time.sleep(1)
-              addBrand()
-              
-          addBrand()
-          
-      else:
-        #Kontroll, et kui sisestatud automarki ei ole, siis sisestab kasutaja uuesti
-          clear()
-          print (userinput + ' meil ei ole')
-          userinput = input("Sisesta automark\n")
+        time.sleep(2)
+        clear()
+
+    print("Programmi töö lõpetatud.")
+        
 run()
